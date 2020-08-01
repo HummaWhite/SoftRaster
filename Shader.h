829a862fc0f6c41948c3bf2801cedc8eaa04e187
HummaWhite <humma@sina.cn>
2020-08-01 23:27:29 +0800

    Shader测试（Lambert光照）

--- Shader.h
+++ Shader.h
@@ -16,23 +16,30 @@ struct SimpleShader
 		{
 			//裁剪阶段插值
 			//通过构造函数生成插值后的实例
-			color = lerp(from.color, to.color, weight);
+			pos = lerp(from.pos, to.pos, weight);
+			texCoord = lerp(from.texCoord, to.texCoord, weight);
+			norm = lerp(from.norm, to.norm, weight);
 		}
 
 		VSToFS(VSToFS& va, VSToFS& vb, VSToFS& vc, Vec3 weight)
 		{
 			//光栅化阶段的三角形重心插值
-			color = triLerp(va.color, vb.color, vc.color, weight);
+			pos = triLerp(va.pos, vb.pos, vc.pos, weight);
+			texCoord = triLerp(va.texCoord, vb.texCoord, vc.texCoord, weight);
+			norm = triLerp(va.norm, vb.norm, vc.norm, weight);
 		}
 
-		Vec4 color;
+		Vec3 pos;
+		Vec2 texCoord;
+		Vec3 norm;
 	};
 
 	// 输入VS的数据类型
 	struct VSIn
 	{
 		Vec3 pos;
-		Vec3 color;
+		Vec2 texCoord;
+		Vec3 norm;
 	};
 
 	// Vertex Shader
@@ -41,18 +48,34 @@ struct SimpleShader
 		Pipeline::VSOut<VSToFS> out;
 
 		Vec4 inPos = { in.pos[0], in.pos[1], in.pos[2], 1.0f };
-
 		out.sr_Position = proj * view * model * inPos;
-		
-		out.data.color = { in.color[0], in.color[1], in.color[2], 1.0f };
+
+		Vec4 outPos = model * inPos;
+
+		Mat3 m(model);
+		Mat3 modelInv = inverse(m).transpose();
+
+		out.data.pos = { outPos[0], outPos[1], outPos[2] };
+		out.data.texCoord = in.texCoord;
+		out.data.norm = (modelInv * in.norm).normalized();
 		return out;
 	}
 
 	// Fragment Shader
 	void processFragment(FrameBufferAdapter& adapter, Pipeline::FSIn<VSToFS> in)
 	{
-		adapter.writeColor(0, in.x, in.y, in.data.color);
-		adapter.writeDepth(in.x, in.y, in.z);
+		Vec3 result(0.0f);
+
+		Vec3 L = { 0.0f, 0.0f, -1.0f };
+		L = L.normalized();
+		Vec3 N = in.data.norm;
+
+		float strength = std::max(0.0f, dot(-L, N)) + 0.05f;
+
+		result = Vec3(strength);
+
+		adapter.writeColor(0, result);
+		adapter.writeDepth(in.z);
 	}
 
 	//uniforms
